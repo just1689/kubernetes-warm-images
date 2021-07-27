efc37673c0fc77b7e427ed408ccaeddd44ee4b51
Just1689 <Just1689@gmail.com>
2021-07-27 12:26:18 +0200

    build  charts - merge with prev

--- helm-build.sh
+++ helm-build.sh
@@ -1,4 +1,4 @@
 helm package warm-images
 mv *.tgz ../charts/
-helm repo index  ../charts/ --url https://storage.googleapis.com/captains-charts/
+helm repo index --merge ../charts/index.yaml  ../charts/ --url https://storage.googleapis.com/captains-charts/
 # TODO: Copy dir to cloud
